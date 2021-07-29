861793078d53afd8bc93b7f232788d0b9145850e
r616on <r616on@gmail.com>
2021-07-30 01:41:50 +0300

    done 1_3

--- src/server.js
+++ src/server.js
@@ -106,7 +106,7 @@ app.use(async (ctx) => {
   }
 });
 
-const server = http.createServer(app.callback()).listen(7070);
+const server = http.createServer(app.callback()).listen(70);
 
 function genId() {
   const id = new Date().getTime();
